7e126355b04d8428070c7c79e4ebac9d5745e8b3
Aidan Thorpe <aidant842@gmail.com>
2021-06-25 12:36:37 +0100

    removed and regenerated key from  app.js, added to env, added state and searching feature for weather app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,62 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-    useEffect(() => {
-        const location = axios.get(
-            "http://api.weatherapi.com/v1/current.json?key=15d8d9de53994c4d9a1102250212506&q=London&aqi=no"
+    //State
+    const [weather, setWeather] = useState(null);
+    const [input, setInput] = useState("");
+    //functions
+    const fetchData = async () => {
+        let response = await axios.get(
+            `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=auto:IP`
         );
+        let localWeather = await axios.get(
+            `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=${response.data.location.name}`
+        );
+        setWeather(localWeather.data);
+    };
+    //useEffect
+    useEffect(() => {
+        fetchData();
     }, []);
-    return <div></div>;
+    //Event
+    const weatherInput = (e) => {
+        setInput(e.target.value);
+    };
+    const searchWeather = async () => {
+        let response = await axios.get(
+            `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=${input}`
+        );
+        setWeather(response.data);
+    };
+    return (
+        <div>
+            {weather && (
+                <div>
+                    <div className="search">
+                        <input onChange={weatherInput} type="text" />
+                        <button onClick={searchWeather}>Search</button>
+                    </div>
+                    <div className="weather-info">
+                        <div className="location">
+                            <h1>
+                                Current weather for {weather.location.region}(
+                                {weather.location.name}),{" "}
+                                {weather.location.country}
+                            </h1>
+                        </div>
+                        <div className="condition">
+                            <h3>{weather.current.condition.text}</h3>
+                            <img
+                                src={weather.current.condition.icon}
+                                alt="Weather Icon"
+                            />
+                            <h3>{weather.current.temp_c}</h3>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
