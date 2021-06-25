c153c12a7a577e03b778daf2b7f88645c410f301
Aidan Thorpe <aidant842@gmail.com>
2021-06-25 11:30:13 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const location = axios.get(
+            "http://api.weatherapi.com/v1/current.json?key=15d8d9de53994c4d9a1102250212506&q=London&aqi=no"
+        );
+    }, []);
+    return <div></div>;
 }
 
 export default App;
